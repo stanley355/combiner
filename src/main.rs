5d11d424806c9ec2e86acc504a48f17944f2fa61
stanley355 <winatastanley355@gmail.com>
2021-12-21 09:59:09 +0700

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-mod collection;
-use collection::{find_mean, find_median, find_mode, to_pig};
+mod trai;
+use trai::{NewsArticle, Tweet};
 
 fn main() {
-    let arr =vec![6, 3, 7, 8, 8, 8]; 
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    find_mean(arr.clone());
-    find_median(arr.clone());
-    find_mode(arr.clone());
-    to_pig("first");
+    println!("1 new tweet: {}", tweet.summarize());
 }
