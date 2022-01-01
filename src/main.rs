c05b4f1faac0a527330b2daf5baee53ffb9aa063
stanley355 <winatastanley355@gmail.com>
2022-01-01 08:44:37 +0700

    Learn about printype

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
-    let v1 = vec![1, 2, 3];
-
-    let mut v1_iter = v1.iter();
-
-    println!("{:?} {:?}", v1_iter.next(), Some(&1));
-    println!("{:?} {:?}",v1_iter.next(), Some(&2));
-    println!("{:?} {:?}",v1_iter.next(), Some(&3));
+    print!("Hello World!");
+    
+    
 }
---
+++ src/rustbyexample/printype.rs
@@ -0,0 +1,17 @@
+pub fn printype() {
+  println!("I'm a rustacean");
+  println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+  println!(
+    "{subject} {verb} {object}",
+    object = "the lazy dog",
+    subject = "the quick brown fox",
+    verb = "jumps over"
+  );
+
+  #[derive(Debug)]
+    struct Structure(i32);
+    println!("This struct {:?} won't print...", Structure(3));
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.2}", pi); //print 3.14
+}
