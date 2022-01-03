47a6bbc32df6861630c09c28a9d3f20a256029d9
stanley355 <winatastanley355@gmail.com>
2022-01-03 20:14:59 +0700

    array and slice

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-// The following struct is for the activity.
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
+use std::mem;
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+}
 
 fn main() {
-    let mat = Matrix(1.1, 1.2, 2.1, 2.2);
+    // Fixed-size array (type signature is superfluous)
+    let xs: [i32; 500] = [1; 500];
 
-    println!("Matrix: ");
-    println!("({:?} {:?})", mat.0, mat.1);
-    println!("({:?} {:?})", mat.2, mat.3);
+    // All elements can be initialized to the same value
+    let ys: [i32; 500] = [0; 500];
+
+    println!("array occupies {:?} bytes", analyze_slice(&xs));
+
+    analyze_slice(&ys[1 .. 4]);
 }
