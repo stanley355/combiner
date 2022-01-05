f8a9639678492be34906fb068c5a4c7dc239f35b
stanley355 <winatastanley355@gmail.com>
2022-01-05 07:51:07 +0700

    rust enum

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-#[derive(Debug)]
-struct Point {
-    x: usize,
-    y: usize,
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
 }
 
-// Structs can be reused as fields of another struct
-#[allow(dead_code)]
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point,
-    bottom_right: Point,
-}
-
-impl Rectangle {
-    fn rect_area(&self) {
-        let area = self.top_left.x * self.bottom_right.y;
-        println!("The size of area is {}", area);
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: usize, y: usize) -> usize {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
     }
 }
 
 fn main() {
-    let point: Point = Point { x: 2, y: 4 };
-
-    let bot = Point { x: 5, ..point };
-
-    let _rectangle = Rectangle {
-        // struct instantiation is an expression too
-        top_left: Point { ..point },
-        bottom_right: bot,
-    };
-
-    _rectangle.rect_area();
+    type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+    let c = Operations::Add;
+    let d = c.run(3, 2);
+    println!("{}", d);
 }
