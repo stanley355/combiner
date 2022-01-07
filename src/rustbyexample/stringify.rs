8840ef0a5f2482b4ae30ecea375ebd5cc4ef9634
stanley355 <winatastanley355@gmail.com>
2022-01-07 09:51:25 +0700

    convert

--- src/rustbyexample/stringify.rs
+++ src/rustbyexample/stringify.rs
@@ -12,5 +12,13 @@ impl fmt::Display for Circle {
 
 fn main() {
     let circle = Circle { radius: 6 };
-    println!("{}", circle);
+    println!("{}", circle.to_string());
+}
+
+fn main_2() {
+  let parsed: i32 = "5".parse().unwrap();
+  let turbo_parsed = "10".parse::<i32>().unwrap();
+
+  let sum = parsed + turbo_parsed;
+  println!("Sum: {:?}", sum);
 }
