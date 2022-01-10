7f67a6c3a400f70c40ec865f63d198ada4e56b5c
stanley355 <winatastanley355@gmail.com>
2022-01-10 10:21:14 +0700

    guard and binding

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,19 @@
+// A function `age` which returns a `u32`.
+fn age() -> u32 {
+    15
+}
+
 fn main() {
-    // Assign a reference of type `i32`. The `&` signifies there
-    // is a reference being assigned.
-    let reference = &4;
+    println!("Tell me what type of person you are");
 
-    // match reference {
-    //     // If `reference` is pattern matched against `&val`, it results
-    //     // in a comparison like:
-    //     // `&i32`
-    //     // `&val`
-    //     // ^ We see that if the matching `&`s are dropped, then the `i32`
-    //     // should be assigned to `val`.
-    //     &val => println!("Got a value via destructuring: {:?}", val),
-    // }
-
-    // To avoid the `&`, you dereference before matching.
-    match reference {
-        val => println!("Got a value via dereferencing: {:?}", val),
-    }
-
-    // // What if you don't start with a reference? `reference` was a `&`
-    // // because the right side was already a reference. This is not
-    // // a reference because the right side is not one.
-    let _not_a_reference = 3;
-
-    // // Rust provides `ref` for exactly this purpose. It modifies the
-    // // assignment so that a reference is created for the element; this
-    // // reference is assigned.
-    let ref _is_a_reference = 3;
-
-    // // Accordingly, by defining 2 values without references, references
-    // // can be retrieved via `ref` and `ref mut`.
-    let value = 5;
-    let mut mut_value = 6;
-
-    // // Use `ref` keyword to create a reference.
-    match value {
-        ref r => println!("Got a reference to a value: {:?}", r),
-    }
-
-    // // Use `ref mut` similarly.
-    match mut_value {
-        ref mut m => {
-            // Got a reference. Gotta dereference it before we can
-            // add anything to it.
-            *m += 10;
-            println!("We added 10. `mut_value`: {:?}", m);
-        },
+    match age() {
+        0             => println!("I haven't celebrated my first birthday yet"),
+        // Could `match` 1 ..= 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 ..= 12. Now the age can be reported.
+        n @ 1  ..= 12 => println!("I'm a child of age {:?}", n),
+        n @ 13 ..= 19 => println!("I'm a teen of age {:?}", n),
+        // Nothing bound. Return the result.
+        n             => println!("I'm an old person of age {:?}", n),
     }
 }
