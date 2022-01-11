63c8c53eaaa3c96619591eba96410ec60cbcdd62
stanley355 <winatastanley355@gmail.com>
2022-01-11 09:00:12 +0700

    let while if

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-// A function `age` which returns a `u32`.
-fn age() -> u32 {
-    15
-}
-
 fn main() {
-    println!("Tell me what type of person you are");
+    // Make `optional` of type `Option<i32>`
+    let mut optional = Some(0);
 
-    match age() {
-        0             => println!("I haven't celebrated my first birthday yet"),
-        // Could `match` 1 ..= 12 directly but then what age
-        // would the child be? Instead, bind to `n` for the
-        // sequence of 1 ..= 12. Now the age can be reported.
-        n @ 1  ..= 12 => println!("I'm a child of age {:?}", n),
-        n @ 13 ..= 19 => println!("I'm a teen of age {:?}", n),
-        // Nothing bound. Return the result.
-        n             => println!("I'm an old person of age {:?}", n),
+    // This reads: "while `let` destructures `optional` into
+    // `Some(i)`, evaluate the block (`{}`). Else `break`.
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+        // ^ Less rightward drift and doesn't require
+        // explicitly handling the failing case.
     }
+    // ^ `if let` had additional optional `else`/`else if`
+    // clauses. `while let` does not have these.
 }
+
