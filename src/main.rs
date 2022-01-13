cfb8988feeed5f82179d80ce0666f97efbde6b07
stanley355 <winatastanley355@gmail.com>
2022-01-13 13:47:29 +0700

    reverse string

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-pub fn production_rate_per_hour(speed: u8) -> f64 {
-    match speed {
-        5..=8 => {
-            println!("hello");
-            let f = speed as f64;
-            (f * 221.0) * 0.9
-        },
-        9..=10 => {
-            let f = speed as f64;
-            (f * 221.0) * 0.77
-        },
-        _ => speed as f64 * 221.0
-    }
+pub fn reverse(input: &str) -> String{
+    input.chars().rev().collect()
 }
 
 fn main() {
-    println!("{}", production_rate_per_hour(8))
+    println!("{}", reverse("aaaab"))
 }
