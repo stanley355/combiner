a82fa9a068e86b771fc3f543bd621495abe926b1
stanley355 <winatastanley355@gmail.com>
2022-01-17 07:41:36 +0700

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
-fn is_odd(n: u32) -> bool {
-    n % 2 == 1
+use time::PrimitiveDateTime as DateTime;
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    let t = start.date();
+    t.checked_add(1000000000.seconds())
+    PrimitiveDateTime::new(date!(2019-01-01), time!(0:00))
 }
 
 fn main() {
-    println!("Find the sum of all the squared odd numbers under 1000");
-    let upper = 1000;
-
-    // Imperative approach
-    // Declare accumulator variable
-    let mut acc = 0;
-    // Iterate: 0, 1, 2, ... to infinity
-    for n in 0.. {
-        // Square the number
-        let n_squared = n * n;
-
-        if n_squared >= upper {
-            // Break loop if exceeded the upper limit
-            break;
-        } else if is_odd(n_squared) {
-            // Accumulate value, if it's odd
-            acc += n_squared;
-        }
-    }
-    println!("imperative style: {}", acc);
-
-    // Functional approach
-    let sum_of_squared_odd_numbers: u32 =
-        (0..).map(|n| n * n)                             // All natural numbers squared
-             .take_while(|&n_squared| n_squared < upper) // Below upper limit
-             .filter(|&n_squared| is_odd(n_squared))     // That are odd
-             .fold(0, |acc, n_squared| acc + n_squared); // Sum them
-    println!("functional style: {}", sum_of_squared_odd_numbers);
+    println!("Hello");
 }
